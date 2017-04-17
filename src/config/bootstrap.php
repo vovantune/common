5503ef069aecda46e593df589fc65b2a4910a055
Vladimir <tune@artskills.ru>
2017-04-17 16:46:25 +0300

    добавил автообъявление константы с версией js

--- src/config/bootstrap.php
+++ src/config/bootstrap.php
@@ -20,4 +20,21 @@ if (file_exists($tableAliasesDir)) {
 
 require_once __DIR__ . '/functions.php';
 
+if (!defined('VERSION_FILE')) {
+	define('VERSION_FILE', CONFIG . 'version.txt');
+}
+
+if (!defined('CORE_VERSION')) {
+	$version = 0;
+	if (file_exists(VERSION_FILE)) {
+		$version = preg_replace('/\D/', '', file_get_contents(VERSION_FILE));
+		if (empty($version)) {
+			$version = 0;
+		}
+	}
+	define('CORE_VERSION', $version);
+	unset($version);
+}
+
+
 \Cake\Database\Type::map('json', \ArtSkills\Database\Type\JsonType::class);
