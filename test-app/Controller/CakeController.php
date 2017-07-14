3f17bd2b797ade13e50d20dfa61c45d32e5e92e5
kalinin <kalinin@artskills-studio.ru>
2017-07-14 12:05:48 +0300

    согласованное поведение при проверке налиция экшнов и шаблонов

---
+++ test-app/Controller/CakeController.php
@@ -0,0 +1,12 @@
+<?php
+namespace TestApp\Controller;
+
+use Cake\Controller\Controller;
+
+class CakeController extends Controller {
+
+	/** Тестируем обращение к экшну с разными регистрами букв */
+	public function testName() {
+	}
+
+}
